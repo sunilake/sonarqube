4aad24300a106ca0ac1e3545b47b620e25705704
David Rautureau <david.rautureau@sonarsource.com>
2018-02-09 16:52:00 +0100

    Only build feature branches (with Gradle)

--- travis.sh
+++ travis.sh
@@ -142,6 +142,8 @@ BUILD)
   export MAVEN_OPTS="-Xmx1G -Xms128m"
   MAVEN_ARGS="-T 1C -Dmaven.test.redirectTestOutputToFile=false -Dsurefire.useFile=false -B -e -V -DbuildVersion=$BUILD_VERSION -Dtests.es.logger.level=WARN -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
 
+  # Minimal Gradle settings
+  export GRADLE_OPTS="-Xmx512m"
 
   # Fetch all commit history so that SonarQube has exact blame information
   # for issue auto-assignment
@@ -153,63 +155,65 @@ BUILD)
 
   if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo 'Build and analyze master'
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
-          $MAVEN_ARGS \
-          -Pdeploy-sonarsource,release
-        
-    mvn sonar:sonar \
-          -Dsonar.host.url=$SONAR_HOST_URL \
-          -Dsonar.login=$SONAR_TOKEN \
-          -Dsonar.projectVersion=$INITIAL_VERSION \
-          -Dsonar.analysis.buildNumber=$BUILD_NUMBER \
-          -Dsonar.analysis.pipeline=$BUILD_NUMBER \
-          -Dsonar.analysis.sha1=$GIT_COMMIT \
-          -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG
+#    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
+#          $MAVEN_ARGS \
+#          -Pdeploy-sonarsource,release
+#        
+#    mvn sonar:sonar \
+#          -Dsonar.host.url=$SONAR_HOST_URL \
+#          -Dsonar.login=$SONAR_TOKEN \
+#          -Dsonar.projectVersion=$INITIAL_VERSION \
+#          -Dsonar.analysis.buildNumber=$BUILD_NUMBER \
+#          -Dsonar.analysis.pipeline=$BUILD_NUMBER \
+#          -Dsonar.analysis.sha1=$GIT_COMMIT \
+#          -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG
 
   elif [[ "$TRAVIS_BRANCH" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo 'Build release branch'
 
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
-        $MAVEN_ARGS \
-        -Pdeploy-sonarsource,release
-
-    mvn sonar:sonar \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_TOKEN \
-        -Dsonar.branch.name=$TRAVIS_BRANCH \
-        -Dsonar.projectVersion=$INITIAL_VERSION \
-        -Dsonar.analysis.buildNumber=$BUILD_NUMBER \
-        -Dsonar.analysis.pipeline=$BUILD_NUMBER \
-        -Dsonar.analysis.sha1=$GIT_COMMIT \
-        -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG
+#    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
+#        $MAVEN_ARGS \
+#        -Pdeploy-sonarsource,release
+#
+#    mvn sonar:sonar \
+#        -Dsonar.host.url=$SONAR_HOST_URL \
+#        -Dsonar.login=$SONAR_TOKEN \
+#        -Dsonar.branch.name=$TRAVIS_BRANCH \
+#        -Dsonar.projectVersion=$INITIAL_VERSION \
+#        -Dsonar.analysis.buildNumber=$BUILD_NUMBER \
+#        -Dsonar.analysis.pipeline=$BUILD_NUMBER \
+#        -Dsonar.analysis.sha1=$GIT_COMMIT \
+#        -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG
   
   elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN:-}" ]; then
     echo 'Build and analyze internal pull request'
 
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
-        $MAVEN_ARGS \
-        -Dsource.skip=true \
-        -Pdeploy-sonarsource
-
-    # TODO remove the sonar.pullrequest.github.* settings after sonar-core-plugins 7.1.0.330 is deployed on Next
-    mvn sonar:sonar \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_TOKEN \
-        -Dsonar.branch.name=$TRAVIS_PULL_REQUEST_BRANCH \
-        -Dsonar.branch.target=$TRAVIS_BRANCH \
-        -Dsonar.analysis.buildNumber=$BUILD_NUMBER \
-        -Dsonar.analysis.pipeline=$BUILD_NUMBER \
-        -Dsonar.analysis.sha1=$TRAVIS_PULL_REQUEST_SHA \
-        -Dsonar.analysis.prNumber=$TRAVIS_PULL_REQUEST \
-        -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.pullrequest.id=$TRAVIS_PULL_REQUEST \
-        -Dsonar.pullrequest.github.id=$TRAVIS_PULL_REQUEST \
-        -Dsonar.pullrequest.github.repository=$TRAVIS_REPO_SLUG
+#    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy \
+#        $MAVEN_ARGS \
+#        -Dsource.skip=true \
+#        -Pdeploy-sonarsource
+#
+#    # TODO remove the sonar.pullrequest.github.* settings after sonar-core-plugins 7.1.0.330 is deployed on Next
+#    mvn sonar:sonar \
+#        -Dsonar.host.url=$SONAR_HOST_URL \
+#        -Dsonar.login=$SONAR_TOKEN \
+#        -Dsonar.branch.name=$TRAVIS_PULL_REQUEST_BRANCH \
+#        -Dsonar.branch.target=$TRAVIS_BRANCH \
+#        -Dsonar.analysis.buildNumber=$BUILD_NUMBER \
+#        -Dsonar.analysis.pipeline=$BUILD_NUMBER \
+#        -Dsonar.analysis.sha1=$TRAVIS_PULL_REQUEST_SHA \
+#        -Dsonar.analysis.prNumber=$TRAVIS_PULL_REQUEST \
+#        -Dsonar.analysis.repository=$TRAVIS_REPO_SLUG \
+#        -Dsonar.pullrequest.id=$TRAVIS_PULL_REQUEST \
+#        -Dsonar.pullrequest.github.id=$TRAVIS_PULL_REQUEST \
+#        -Dsonar.pullrequest.github.repository=$TRAVIS_REPO_SLUG
 
   else
     echo 'Build feature branch or external pull request'
 
-    mvn deploy $MAVEN_ARGS -Dsource.skip=true -Pdeploy-sonarsource
+#    mvn deploy $MAVEN_ARGS -Dsource.skip=true -Pdeploy-sonarsource
+     
+    ./gradlew build publishToMavenLocal artifactory --no-daemon
   fi
 
   ./run-integration-tests.sh "Lite" ""
